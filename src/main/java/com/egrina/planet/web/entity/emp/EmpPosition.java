a073250befd1e2d8a4fdbe292b2c2d87a42ed370
kimhs <kimhs@ais-info.co.jp>
2013-05-11 21:55:08 +0900

    大文字→小文字変更。

--- src/main/java/com/egrina/planet/web/entity/emp/EmpPosition.java
+++ src/main/java/com/egrina/planet/web/entity/emp/EmpPosition.java
@@ -1,7 +1,6 @@
 package com.egrina.planet.web.entity.emp;
 
 import java.util.Calendar;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,7 +8,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.egrina.planet.web.entity.basic.*;
@@ -21,7 +19,7 @@ public class EmpPosition {
     @Id
     @GeneratedValue
     @Column(name = "EMP_POSITION_PK")
-    private Integer EmpPositionPk;
+    private Integer empPositionPk;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "EMP_CODE", nullable = false)
@@ -41,11 +39,11 @@ public class EmpPosition {
     }
 
     public Integer getEmpPositionPk() {
-        return EmpPositionPk;
+        return empPositionPk;
     }
 
     public void setEmpPositionPk(Integer empPositionPk) {
-        EmpPositionPk = empPositionPk;
+        this.empPositionPk = empPositionPk;
     }
 
     public BasicPosition getBasicPosition() {
